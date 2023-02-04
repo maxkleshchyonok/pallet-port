303bcb98334bb267bfea858535fc2ea93108db1c
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-02-04 23:13:32 +0300

    feat: add catalog & item pages

--- src/core/types/types.ts
+++ src/core/types/types.ts
@@ -1,20 +1,60 @@
-// export type ProductType = {
-//   id: number;
-//   short: string;
-//   name: string;
-//   category: string;
-//   condition: string;
-//   material: string;
-//   length: number;
-//   width: number;
-//   height: number;
-//   load: number;
-//   image1: string;
-//   image2: string;
-//   info: string;
-//   price: number;
-//   quantity: number;
-// };
+export type ProductType = {
+  id: number;
+  short: string;
+  name: string;
+  category: string;
+  condition: string;
+  material: string;
+  length: number;
+  width: number;
+  height: number;
+  load: number;
+  image1: string;
+  image2: string;
+  info: string;
+  price: number;
+  quantity: number;
+};
+
+export enum SortEnum {
+  DEFAULT = 'Wybrane',
+  NAME = 'Udźwig rosnąco',
+  NAME_REVERSED = 'Udźwig malejąco ',
+  PRICE_UP = 'Najtańsze',
+  PRICE_DOWN = 'Najdroższe',
+}
+
+export interface IFilters {
+  price: [number, number];
+  // stock: boolean;
+  category: string[];
+  // material: string[];
+  // length: [number, number] | [];
+  // width: [number, number] | [];
+  // height: [number, number] | [];
+  // load: [number, number] | [];
+  // sort: SortEnum;
+  // quantity: number;
+}
+
+export const INITIAL_STATE = {
+  price: [0, 500] as [number, number],
+  category: ['palety_euro', 'palety_europodobne', 'palety_jedno',
+    'palety_przem', 'polpalety', 'palety_tektur', 'palety_plastik', 'nadstawki'],
+  condition: ['used', 'new'],
+  quantity: [0, 100000],
+  material: ['drewno', 'plastik', 'tektura'],
+  length: [0, 3000],
+  width: [0, 3000],
+  height: [0, 3000],
+  load: [0, 5000],
+  sort: SortEnum.DEFAULT,
+  short: ['euro_new', 'euro_used_1', 'euro_used_2', 'euro_used_3',
+    'europod_new', 'europod_used', 'jedno_new_1', 'jedno_new_2',
+    'jedno_used_1', 'jedno_used_2', 'jedno_used_3', 'przem_1', 'przem_2',
+    'polpal_1', 'polpal_2', 'plastik_1', 'plastik_2', 'tektur_1',
+    'nadstawka_1', 'nadstawka_2', 'nadstawka_3'],
+};
 
 /**
  *
--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,48 +1,146 @@
-import Page from '../../core/templates/page';
-import MainPage from '../main';
-import Header from '../../core/components/header';
-import ErrorPage, { ErrorTypes } from '../error';
-import CatalogPage from '../catalog';
-import CartPage from '../cart';
-import AccountPage from '../account';
+// import Page from '../../core/templates/page';
+// import MainPage from '../main';
+// import Header from '../../core/components/header';
+// import ErrorPage, { ErrorTypes } from '../error';
+// import CatalogPage from '../catalog';
+// import CartPage from '../cart';
+// import AccountPage from '../account';
+//
+// export const enum PageIds {
+//   Main_page = 'main-page',
+//   Catalog_page = 'catalog-page',
+//   Cart_page = 'cart-page',
+//   Account_page = 'account-page',
+// }
+//
+// class App {
+//   private static container: HTMLElement | null = document.getElementById('content');
+//
+//   public static defaultPageId = 'main-page';
+//
+//   private header: Header;
+//
+//
+//   static renderNewPage(idPage: string) {
+//
+//
+//     const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+//     if (currentPageHTML) {
+//       currentPageHTML.remove();
+//     }
+//     let page: Page | null;
+//
+//     if (idPage === PageIds.Main_page) {
+//       page = new MainPage(idPage);
+//     } else if (idPage === PageIds.Catalog_page) {
+//       page = new CatalogPage(idPage);
+//     } else if (idPage === PageIds.Cart_page) {
+//       page = new CartPage(idPage);
+//     } else if (idPage === PageIds.Account_page) {
+//       page = new AccountPage(idPage);
+//     } else {
+//       page = new ErrorPage(idPage, ErrorTypes.Error_404);
+//     }
+//
+//     if (page) {
+//       const pageHTML = page.render();
+//       pageHTML.id = App.defaultPageId;
+//       App.container?.append(pageHTML);
+//     }
+//   }
+//
+//   private enableRouteChange() {
+//     window.addEventListener('hashchange', () => {
+//       const hash = window.location.hash.slice(1);
+//       App.renderNewPage(hash);
+//     });
+//   }
+//
+//
+//   constructor() {
+//     this.header = new Header('header', 'header-container');
+//   }
+//
+//   run() {
+//     App.container?.append(this.header.render());
+//     App.renderNewPage('main-page');
+//     this.enableRouteChange();
+//   }
+// }
+//
+// export default App;
 
-export const enum PageIds {
-  Main_page = 'main-page',
-  Catalog_page = 'catalog-page',
-  Cart_page = 'cart-page',
-  Account_page = 'account-page',
-}
+
+
+import MainPage from '../main';
+import CatalogPage from '../catalog';
+import Page from '../../core/templates/page';
+import CartPage from '../cart';
+import Header from '../../core/components/header';
+import ProductPage from '../product-page';
+// import Footer from '../../core/components/footer';
+import { loadParameters, parametersObj } from '../../core/components/parameters';
+// import createProductCard from '../../core/components/product_card/product_card';
+import productsJSON from '../../assets/json/products.json';
+import Product from '../../core/components/product/product';
+import ErrorPage from '../error';
+
+const products: Product[] = productsJSON;
+const productsId: string[] = [];
+products.forEach((product) => {
+  productsId.push(`product-page/${product.id}`);
+});
+
+export const PageIds: { [props: string]: string | string[] } = {
+  MainPageId: 'main-page',
+  CatalogPageId:'catalog-page',
+  CartPageId: 'cart-page',
+  ErrorPage: 'error-page',
+  ProductPageId: productsId,
+};
 
 class App {
-  private static container: HTMLElement | null = document.getElementById('content');
+  static container: HTMLElement | null = document.getElementById('content');
 
-  private static defaultPageId = 'main-page';
+  static defaultPageId = 'catalog-page';
+
+  private initialPage: MainPage;
 
   private header: Header;
 
+  // private footer: Footer;
 
-  static renderNewPage(idPage: string) {
+  // previousPage: string[] = [];
+  previousPage = '';
 
-
-    const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+  public renderNewPage(idPage: string) {
+    const currentPageHTML = document.getElementById(App.defaultPageId);
     if (currentPageHTML) {
       currentPageHTML.remove();
     }
-    let page: Page | null;
-
-    if (idPage === PageIds.Main_page) {
+    let page: Page | null = null;
+    if (idPage === PageIds.MainPageId) {
       page = new MainPage(idPage);
-    } else if (idPage === PageIds.Catalog_page) {
+    } else if (idPage === PageIds.CatalogPageId) {
       page = new CatalogPage(idPage);
-    } else if (idPage === PageIds.Cart_page) {
+    } else if (idPage === PageIds.CartPageId) {
       page = new CartPage(idPage);
-    } else if (idPage === PageIds.Account_page) {
-      page = new AccountPage(idPage);
+    } else if (PageIds.ProductPageId.includes(idPage)) {
+      const id = Number(idPage.replace(/[\D]+/g, ''));
+      const product = products.find((el) => el.id === id);
+      if (product !== undefined) {
+        parametersObj(product.short);
+        // saveParameters();
+        page = new ProductPage(idPage);
+      } else {
+        page = new ErrorPage(idPage, '404');
+      }
     } else {
-      page = new ErrorPage(idPage, ErrorTypes.Error_404);
+      page = new ErrorPage(idPage, '404');
     }
 
     if (page) {
+      this.previousPage = window.location.hash.slice(1);
       const pageHTML = page.render();
       pageHTML.id = App.defaultPageId;
       App.container?.append(pageHTML);
@@ -50,21 +148,47 @@ class App {
   }
 
   private enableRouteChange() {
-    window.addEventListener('hashchange', () => {
+    const loadPage = () => {
       const hash = window.location.hash.slice(1);
-      App.renderNewPage(hash);
+      loadParameters();
+      if (!hash) {
+        window.location.hash = 'main-page';
+      }
+      if (!hash.includes('?')) {
+        this.renderNewPage(hash);
+      } else {
+        if (this.previousPage.slice(0, hash.indexOf('?')) === hash.slice(0, hash.indexOf('?'))) {
+          setTimeout(() => {}, 1);
+        } else {
+          this.renderNewPage(`${hash.slice(0, hash.indexOf('?'))}`);
+        }
+      }
+
+    };
+    window.addEventListener('hashchange', () => {
+      if (window.location.hash.includes('?') && !this.previousPage[this.previousPage.length - 1].includes('catalog')) {
+        this.renderNewPage('catalog-page');
+      } else
+        loadPage();
+    });
+    window.addEventListener('load', () => {
+      loadPage();
     });
   }
 
 
   constructor() {
     this.header = new Header('header', 'header-container');
+    this.initialPage = new MainPage('main-page');
+    // this.footer = new Footer('footer', 'footer-container');
   }
 
   run() {
     App.container?.append(this.header.render());
-    App.renderNewPage('main-page');
+    this.renderNewPage('main-page');
+    window.location.hash = PageIds.MainPageId as string;
     this.enableRouteChange();
+    // App.container?.append(this.footer.render());
   }
 }
 
