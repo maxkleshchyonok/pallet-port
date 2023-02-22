92ba3b0d5acd104614c5fab43aea410e47f75839
Vitaly Vegera <surewek@gmail.com>
2023-02-22 23:40:17 +0300

    feat: add product category service

--- backend/src/controllers/product-сategory-controller.ts
+++ backend/src/controllers/product-сategory-controller.ts
@@ -2,20 +2,39 @@ import productCategoryService from '../services/product-сategory-service.js';
 
 class ProductCategoryController {
     async createProductCategory(request: any, response: any) {
-        const productCategory = await productCategoryService.create(request.body);
-        response.json(productCategory);
+        try {
+            const productCategory = await productCategoryService.create(request.body);
+            response.json(productCategory);
+        } catch (error) {
+            console.error('Create error');
+        }
     };
 
     async getProductCategory(request: any, response: any) {
-
+        try {
+            const productCategory = await productCategoryService.get(request.params.id);
+            response.json(productCategory);
+        } catch (error) {
+            console.error('Get error');
+        }
     };
 
     async updateProductCategory(request: any, response: any) {
-
+        try {
+            const productCategory = await productCategoryService.update(request.params.id, request.body);
+            response.json(productCategory);
+        } catch (error) {
+            console.error('Update error');
+        }
     };
 
     async deleteProductCategory(request: any, response: any) {
-
+        try {
+            const productCategory = await productCategoryService.delete(request.params.id);
+            response.json(productCategory);
+        } catch (error) {
+            console.error('Delete error');
+        }
     };
 }
 
--- backend/src/services/product-сategory-service.ts
+++ backend/src/services/product-сategory-service.ts
@@ -6,6 +6,21 @@ class ProductCategoryService {
         const newProductCategory = await ProductCategory.create({ name, description, image, shortName })
         return newProductCategory;
     };
+
+    async get(id: string) {
+        const newProductCategory = await ProductCategory.findById(id);
+        return newProductCategory;
+    };
+
+    async update(id: string, productCategory: any) {
+        const updatedProductCategory = await ProductCategory.findByIdAndUpdate(id, productCategory, { new: true });
+        return updatedProductCategory;
+    };
+
+    async delete(id: string) {
+        const productCategory = await ProductCategory.findByIdAndDelete(id);
+        return productCategory;
+    };
 }
 
 export default new ProductCategoryService();
