b61e6464f90113ef494ba4b23f8001246124f4d7
Vitaly Vegera <surewek@gmail.com>
2023-02-23 15:58:36 +0300

    feat: add product service

--- backend/src/controllers/product-controller.ts
+++ backend/src/controllers/product-controller.ts
@@ -2,20 +2,39 @@ import productService from '../services/product-service.js';
 
 class ProductController {
     async createProduct(request: any, response: any) {
-        const product = await productService.create(request.body);
-        response.json(product);
+        try {
+            const product = await productService.create(request.body);
+            response.json(product);
+        } catch (error) {
+            console.error('Create error');
+        }
     };
 
     async getProduct(request: any, response: any) {
-
+        try {
+            const product = await productService.get(request.params.id);
+            response.json(product);
+        } catch (error) {
+            console.error('Get error');
+        }
     };
 
     async updateProduct(request: any, response: any) {
-
+        try {
+            const product = await productService.update(request.params.id, request.body);
+            response.json(product);
+        } catch (error) {
+            console.error('Update error');
+        }
     };
 
     async deleteProduct(request: any, response: any) {
-
+        try {
+            const product = await productService.delete(request.params.id);
+            response.json(product);
+        } catch (error) {
+            console.error('Delete error');
+        }
     };
 }
 
--- backend/src/services/product-service.ts
+++ backend/src/services/product-service.ts
@@ -3,8 +3,23 @@ import Product from "../schemas/product.js";
 class ProductService {
     async create(product: any) {
         const { name, material, condition, image1, image2, shortName, length, width, height, maxLoad, category } = product;
-        const newProductCategory = await Product.create({ name, material, condition, image1, image2, shortName, length, width, height, maxLoad, category })
-        return newProductCategory;
+        const newProduct = await Product.create({ name, material, condition, image1, image2, shortName, length, width, height, maxLoad, category });
+        return newProduct;
+    };
+
+    async get(id: string) {
+        const product = await Product.findById(id);
+        return product;
+    };
+
+    async update(id: string, product: any) {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        return updatedProduct;
+    };
+
+    async delete(id: string) {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        return deletedProduct;
     };
 }
 
