595ec4c3f727e93058be4ddbc5e964e778f9ca44
Vitaly Vegera <surewek@gmail.com>
2023-02-24 12:01:19 +0300

    feat: add order service

--- backend/src/controllers/order-controller.ts
+++ backend/src/controllers/order-controller.ts
@@ -1,22 +1,59 @@
+import orderService from '../services/order-service.js';
+
 class OrderController {
     async createOrder(request: any, response: any) {
-
+        try {
+            const order = await orderService.create(request.body);
+            response.json(order);
+        } catch (error) {
+            console.error('Create error');
+        }
     };
 
     async getOrder(request: any, response: any) {
-
+        try {
+            const order = await orderService.get(request.params.id);
+            console.log(request.query.status, '00');
+            response.json(order);
+        } catch (error) {
+            console.error('Get one error');
+        }
     };
 
     async updateOrder(request: any, response: any) {
-
+        try {
+            const order = await orderService.update(request.params.id, request.body);
+            response.json(order);
+        } catch (error) {
+            console.error('Update error');
+        }
     };
 
     async deleteOrder(request: any, response: any) {
-
+        try {
+            const order = await orderService.delete(request.params.id);
+            response.json(order);
+        } catch (error) {
+            console.error('Delete error');
+        }
     };
 
-    async getSortedOrders(request: any, response: any) {
+    async getByStatus(request: any, response: any) {
+        try {
+            const sortedOrders = await orderService.sortByStatus(request.query.status);
+            response.json(sortedOrders);
+        } catch (error) {
+            console.error('Get sorted by status error');
+        }
+    };
 
+    async getByUser(request: any, response: any) {
+        try {
+            const sortedOrders = await orderService.sortByUser(request.query.email);
+            response.json(sortedOrders);
+        } catch (error) {
+            console.error('Get sorted by user error');
+        }
     };
 }
 
--- backend/src/routers/order-router.ts
+++ backend/src/routers/order-router.ts
@@ -7,6 +7,7 @@ orderRouter.post('/orders', orderController.createOrder);
 orderRouter.get('/orders/:id', orderController.getOrder);
 orderRouter.post('/orders/:id', orderController.updateOrder);
 orderRouter.delete('/orders/:id', orderController.deleteOrder);
-orderRouter.get('/orders/findByStatus', orderController.getSortedOrders);
+orderRouter.get('/orders/find/findByStatus', orderController.getByStatus);
+orderRouter.get('/orders/find/findByUser/', orderController.getByUser);
 
 export default orderRouter;
