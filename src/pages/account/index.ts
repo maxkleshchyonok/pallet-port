6e384259f503d7e9f1ee3dfdcf8244f4f2b14ada
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-02-25 19:03:52 +0300

    feat: add account delete button

--- src/pages/account/index.ts
+++ src/pages/account/index.ts
@@ -100,6 +100,7 @@ class AccountPage extends Page {
       const ratingTitle = document.createElement('h3');
       const ratingNumber = document.createElement('h1');
       const saveButton = document.createElement('button');
+      const deleteButton = document.createElement('button');
 
       avatarBlock.className = 'user__avatar';
       avatar.className = 'avatar__image';
@@ -110,6 +111,7 @@ class AccountPage extends Page {
       ratingTitle.className = 'rating__title';
       ratingNumber.className = 'rating__number';
       saveButton.className = 'user__saveButton';
+      deleteButton.className = 'user__deleteButton';
 
       avatar.src = '../../assets/img/elements/account-user.svg';
       editAvatar.src = '../../assets/img/elements/edit-avatar.svg';
@@ -118,6 +120,7 @@ class AccountPage extends Page {
       ratingNumber.textContent = '5';
 
       saveButton.textContent = 'Zapisz';
+      deleteButton.textContent = 'Usuń konto';
 
       roles.append(rolesTitle);
 
@@ -146,7 +149,7 @@ class AccountPage extends Page {
 
       avatarBlock.append(avatar, editAvatar);
       ratingBlock.append(ratingTitle, ratingNumber);
-      user.append(avatarBlock, roles, ratingBlock, saveButton);
+      user.append(avatarBlock, roles, ratingBlock, saveButton, deleteButton);
 
     }
 
@@ -221,6 +224,24 @@ class AccountPage extends Page {
 
 
       main.append(firmaBlock, addressBlock);
+    }
+
+    function renderOrdersBlock(): void {
+      const searchBlock = document.createElement('div');
+      const searchTitle = document.createElement('h2');
+      const searchField = document.createElement('div');
+      const searchInput = document.createElement('input');
+      const searchButton = document.createElement('button');
+      const categoryBlock = document.createElement('div');
+      const orders = document.createElement('div');
+
+      searchBlock.className = 'search';
+      searchTitle.className = 'search__title';
+      searchField.className = 'search__field';
+      searchInput.className = 'search__input';
+      searchButton.className = 'search__button';
+      categoryBlock.className = 'categories';
+      orders.className = 'orders';
 
     }
 
@@ -283,6 +304,8 @@ class AccountPage extends Page {
           clearContent();
           renderFirmsBlock();
           renderUserBlock();
+        } else if (listItem.id === '2') {
+          renderOrdersBlock();
         }
 
       });
