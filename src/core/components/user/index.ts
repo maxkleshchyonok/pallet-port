1aab6548d843febc0c7f8074f3eff6a623acb184
maxkleshchyonok <56303675+maxkleshchyonok@users.noreply.github.com>
2023-03-02 02:27:15 +0300

    feat: add create order request

--- src/core/components/user/index.ts
+++ src/core/components/user/index.ts
@@ -2,7 +2,6 @@ import { User } from '../../types/types';
 
 export default class MyUser implements User {
   constructor(
-    public name: User['name'],
     public email: User['email'],
     public password: User['password'],
   ) {
